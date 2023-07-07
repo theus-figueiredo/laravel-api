f536afc40c29e72e19ac7c677f3416e977d760b4
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-07-07 11:44:59 -0300

    definido relacionamento entre real state e photos

--- app/Http/Controllers/Api/RealStatePhotoController.php
+++ app/Http/Controllers/Api/RealStatePhotoController.php
@@ -6,6 +6,7 @@ use App\Api\ApiMessages;
 use App\Http\Controllers\Controller;
 use App\Models\RealStatePhoto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RealStatePhotoController extends Controller
 {
@@ -18,12 +19,14 @@ class RealStatePhotoController extends Controller
 
     public function setThumb(string $photoId, string $realStateId) {
         try {
-            $photo = $this->realStatePhoto->where('is_thumb', true)->where('real_state_id', $realStateId)->first();
+            $photo = $this->realStatePhoto->where('is_thumb', true)->where('real_state_id', $realStateId);
 
-            if ($photo->count()) $photo->update(['is_thumb' => false]);
+            if ($photo->count()) $photo->first()->update(['is_thumb' => false]);
 
             $photo = $this->realStatePhoto->find($photoId);
             $photo->update(['is_thumb' => true]);
+
+            return response()->json(['data' => ['msg' => 'Thumbnail definida com sucesso']], 200);
 
         } catch (\Exception $exception) {
             $message = new ApiMessages($exception->getMessage());
@@ -31,7 +34,26 @@ class RealStatePhotoController extends Controller
         }
     }
 
-    public function removePhoto($photoId) {
 
+    public function removePhoto($photoId) {
+        try {
+            $photo = $this->realStatePhoto->find($photoId);
+
+            if($photo->is_thumb) {
+                $message = new ApiMessages('Não é possível remover thumbnail');
+                return response()->json(['Error' => $message->getMessage()], 401);
+            }
+
+            if ($photo) {
+                Storage::disk('public')->delete($photo->photo);
+                $photo->delete();
+            }
+
+            return response()->json(['data' => ['msg' => 'imagem removida']]);
+
+        } catch (\Exception $exception) {
+            $message = new ApiMessages($exception->getMessage());
+            return response()->json(['Error' => $message->getMessage()]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RealStateController;
+use App\Http\Controllers\Api\RealStatePhotoController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -37,5 +38,11 @@ Route::prefix('v1')->namespace('App\Http\Controllers\Api')->group(function() {
     Route::name('categories.')->group(function() {
         Route::get('/categories/{id}/real-states', [CategoryController::class, 'realState']);
         Route::resource('/categories', CategoryController::class); //api/v1/categories/
+    });
+
+    
+    Route::name('photos.')->prefix('photos')->group(function() {
+        Route::delete('/{id}', [RealStatePhotoController::class, 'removePhoto'])->name('delete'); //api/v1/photos/{id}/
+        Route::put('/set-thumb/{photoId}/{realStateId}', [RealStatePhotoController::class, 'setThumb']);//api/v1/photos/set-thumb/{photoId}/{realStateId}/
     });
 });
